0829e9f88c79c4a5da4e507050c1c1fe3fa44797
m00nc87 <softboymoon@gmail.com>
2020-08-21 20:00:21 +0300

    "done task"

--- wp-content/themes/twentynineteen/index.php
+++ wp-content/themes/twentynineteen/index.php
@@ -1,8 +1,120 @@
 <link rel='stylesheet' href='/wp-content/themes/twentynineteen/dist/style.css'>
 <script type='text/javascript' src='/wp-content/themes/twentynineteen/dist/index.js'></script>
-
-<div class="hello">Hello - Please make page here!</div>
-
+<div class="header">
+    <div class="header__logo">
+        <a href="#"><img src="<?php bloginfo('template_url'); ?>/image/logo.png" alt="logo"></a>
+    </div>
+    <div class="header__menu">
+        <ul>
+            <li><a href="#">What we do</a></li>
+            <li><a href="#">Our work</a></li>
+            <li><a href="#">Seo insights</a></li>
+            <li><a href="#">About us</a></li>
+        </ul>
+    </div>
+</div>
+<div class="jumbotron">
+    <div class="jumbotron__message-block">
+        <div class="jumbotron__title">
+            <h1>Workforce Survey</h1>
+        </div>
+        <div class="jumbotron__descr">
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+        </div>
+        <button><a href="#">Start Now</a></button>
+    </div>
+</div>
+<div class="wrapper">
+    <div class="cards">
+        <div class="cards__item">
+            <div class="cards__img">
+                <img src="<?php bloginfo('template_url'); ?>/image/card.png" alt="card">
+            </div>
+            <div class="cards__body">
+                <div class="cards__title">
+                    <h3>
+                        Kym Marsh, 42, becomes a grandmother for the first time as daughter Emilie Mae
+                    </h3>
+                </div>
+                <div class="cards__descr">
+                    <p>
+                        molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui <span class="more">officia deserunt molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt</span>
+                    </p>
+                </div>
+                <div class="cards__btn">
+                    <img src="<?php bloginfo('template_url'); ?>/image/arrow.svg" alt="arrow"><span>Show more</span>
+                </div>
+            </div>
+        </div>
+        <div class="cards__item">
+            <div class="cards__img">
+                <img src="<?php bloginfo('template_url'); ?>/image/card.png" alt="card">
+            </div>
+            <div class="cards__body">
+                <div class="cards__title">
+                    <h3>
+                        Kym Marsh, 42, becomes a grandmother for the first time as daughter Emilie Mae
+                    </h3>
+                </div>
+                <div class="cards__descr">
+                    <p>
+                        molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui <span class="more">officia deserunt molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt</span>
+                    </p>
+                </div>
+                <div class="cards__btn">
+                    <img src="<?php bloginfo('template_url'); ?>/image/arrow.svg" alt="arrow"><span>Show more</span>
+                </div>
+            </div>
+        </div>
+        <div class="cards__item">
+            <div class="cards__img">
+                <img src="<?php bloginfo('template_url'); ?>/image/card.png" alt="card">
+            </div>
+            <div class="cards__body">
+                <div class="cards__title">
+                    <h3>
+                        Kym Marsh, 42, becomes a grandmother for the first time as daughter Emilie Mae
+                    </h3>
+                </div>
+                <div class="cards__descr">
+                    <p>
+                        molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui <span class="more">officia deserunt molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt</span>
+                    </p>
+                </div>
+                <div class="cards__btn">
+                    <img src="<?php bloginfo('template_url'); ?>/image/arrow.svg" alt="arrow"><span>Show more</span>
+                </div>
+            </div>
+        </div>
+        <div class="cards__item">
+            <div class="cards__img">
+                <img src="<?php bloginfo('template_url'); ?>/image/card.png" alt="card">
+            </div>
+            <div class="cards__body">
+                <div class="cards__title">
+                    <h3>
+                        Kym Marsh, 42, becomes a grandmother for the first time as daughter Emilie Mae
+                    </h3>
+                </div>
+                <div class="cards__descr">
+                    <p>
+                        molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui <span class="more">officia deserunt molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt</span>
+                    </p>
+                </div>
+                <div class="cards__btn">
+                    <img src="<?php bloginfo('template_url'); ?>/image/arrow.svg" alt="arrow"><span>Show more</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="footer">
-  2000-<?php echo $created_at; ?>
+    <div class="footer__cop">
+        Ukraine 2000-<?php echo $created_at; ?>
+    </div>
+    <div class="footer__socials">
+        <a><img src="<?php bloginfo('template_url'); ?>/image/fb-2.png" alt="facebook"></a>
+        <a><img src="<?php bloginfo('template_url'); ?>/image/tw.png" alt="twitter"></a>
+        <a><img src="<?php bloginfo('template_url'); ?>/image/in.png" alt="instagram"></a>
+    </div>
 </div>
--- wp-content/themes/twentynineteen/js/index.js
+++ wp-content/themes/twentynineteen/js/index.js
@@ -4,3 +4,20 @@ import * as JQuery from "jquery";
 window.$ = JQuery.default;
 
 console.log('Hello!');
+
+
+$(document).ready(function(){
+    $(".more").toggle();
+    $(".cards__btn").click(function(){
+        let item = $(this).parent();
+        $(".more", item).toggle();
+        if($(".more", item).is(":visible")){
+            $("span", this).text('Show less');
+            $(this).addClass("cards__btn_toogled");
+        }else{
+            $("span", this).text('Show more');
+            $(this).removeClass("cards__btn_toogled");
+        }
+        toogled = !toogled;
+    });
+});
